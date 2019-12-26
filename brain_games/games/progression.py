9db286bd719c6896d8d797a21bd917ef61f1217a
Uladzislau Vasilyeu <VSvasilyev97@gmail.com>
2019-12-26 20:55:11 +0300

    Refactor generating of progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -8,32 +8,25 @@ MAX_PROGRESSION_INDEX = 9
 MIN_COMMON_DIFFERENCE = 1
 
 
-def generate_sequence(first_element, common_difference):
-    progression = []
-    for i in range(PROGRESSION_LENGTH):
-        progression.append(first_element + common_difference * i)
-    return progression
-
-
-def generate_sequence_with_hidden_element(sequence, hidden_element_index):
-    result = sequence[:]
-    result[hidden_element_index] = ".."
-    return result
-
-
 def generate_task():
     first_element = generate_random_number()
     common_difference = generate_random_number(MIN_COMMON_DIFFERENCE)
-    sequence = generate_sequence(first_element, common_difference)
     hidden_element_index = generate_random_number(
         MIN_PROGRESSION_INDEX,
         MAX_PROGRESSION_INDEX
     )
+
+    sequence = []
+    sequence_with_hidden_element = []
+    for i in range(PROGRESSION_LENGTH):
+        element = first_element + common_difference * i
+        sequence.append(element)
+        if i == hidden_element_index:
+            sequence_with_hidden_element.append("..")
+        else:
+            sequence_with_hidden_element.append(element)
+
     correct_answer = str(sequence[hidden_element_index])
-    sequence_with_hidden_element = generate_sequence_with_hidden_element(
-        sequence,
-        hidden_element_index
-    )
     question = " ".join(list(map(str, sequence_with_hidden_element)))
     return {
         "question": question,
