ec30a42c217711f7a9342acff080195ef6054aa8
Uladzislau Vasilyeu <VSvasilyev97@gmail.com>
2019-12-26 20:35:22 +0300

    Update the way of generating of the arithmetic operations for calc

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,28 +1,21 @@
 from brain_games.utils import generate_random_number
+import random
+import operator
 
-MIN_SIGN_INDEX = 0
-MAX_SIGN_INDEX = 2
 GAME_DESCRIPTION = "What is the result of the expression?"
 
-arithmetic_signs = ["+", "-", "*"]
-arithmetic_operations_by_sign = {
-    "+": lambda first_num, second_num: first_num + second_num,
-    "-": lambda first_num, second_num: first_num - second_num,
-    "*": lambda first_num, second_num: first_num * second_num
-}
-
-
-def generate_random_arithmetic_sign():
-    random_index = generate_random_number(MIN_SIGN_INDEX, MAX_SIGN_INDEX)
-    return arithmetic_signs[random_index]
+arithmetic_operations = [
+    ("+", operator.add),
+    ("-", operator.sub),
+    ("*", operator.mul)
+]
 
 
 def generate_task():
     first_num = generate_random_number()
     second_num = generate_random_number()
-    sign = generate_random_arithmetic_sign()
+    sign, operation = random.choice(arithmetic_operations)
     question = "{} {} {}".format(first_num, sign, second_num)
-    operation = arithmetic_operations_by_sign[sign]
     correct_answer = str(operation(first_num, second_num))
     return {
         "question": question,
