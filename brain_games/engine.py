6a2a7dd2263223eafa4a5304029fa8f2797c8827
Uladzislau Vasilyeu <VSvasilyev97@gmail.com>
2019-12-26 20:06:23 +0300

    Combine engine ask_question and run methods

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -3,10 +3,18 @@ import prompt
 MAX_NUMBER_OF_QUESTIONS = 3
 
 
-def ask_question(generate_task):
+def run(game):
+    print("Welcome to the Brain Games!")
+    print(game.GAME_DESCRIPTION)
+    print()
+
+    name = prompt.string("May I have your name? ")
+    print("Hello, {}!".format(name))
+    print()
+
     question_number = 1
     while question_number <= MAX_NUMBER_OF_QUESTIONS:
-        task_data = generate_task()
+        task_data = game.generate_task()
         question = task_data["question"]
         correct_answer = task_data["correct_answer"]
 
@@ -14,28 +22,11 @@ def ask_question(generate_task):
         given_answer = prompt.string("Your answer: ")
 
         if (given_answer != correct_answer):
-            return {
-                "is_victory": False,
-                "given_answer": given_answer,
-                "correct_answer": correct_answer
-            }
+            result = "'{}' is wrong answer ;(. Correct answer was '{}'."
+            print(result.format(given_answer, correct_answer))
+            return
 
         print("Correct!")
         question_number += 1
 
-    return {"is_victory": True}
-
-
-def run(game):
-    print("Welcome to the Brain Games!")
-    print(game.GAME_DESCRIPTION + "\n")
-    name = prompt.string("May I have your name? ")
-    print("Hello, {}!\n".format(name))
-    game_result = ask_question(game.generate_task)
-    if game_result["is_victory"]:
-        print("Congratulations, {}!".format(name))
-    else:
-        answer_template = "'{}' is wrong answer ;(. Correct answer was '{}'."
-        given_answer = game_result["given_answer"]
-        correct_answer = game_result["correct_answer"]
-        print(answer_template.format(given_answer, correct_answer))
+    print("Congratulations, {}!".format(name))
